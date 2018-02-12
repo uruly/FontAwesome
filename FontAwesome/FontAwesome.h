c8eaf5b08ce08340d9b1403102062164ea3a5ef1
Reona Kubo <rk.uru007@gmail.com>
2018-02-12 19:27:05 +0900

    Initial Commit

---
+++ FontAwesome/FontAwesome.h
@@ -0,0 +1,19 @@
+//
+//  FontAwesome.h
+//  FontAwesome
+//
+//  Created by Reona Kubo on 2018/02/12.
+//  Copyright © 2018年 Reona Kubo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FontAwesome.
+FOUNDATION_EXPORT double FontAwesomeVersionNumber;
+
+//! Project version string for FontAwesome.
+FOUNDATION_EXPORT const unsigned char FontAwesomeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FontAwesome/PublicHeader.h>
+
+
